de3137280536f26b4e9eb1c343faef11510f727f
jmredfern-ar <46031172+jmredfern-ar@users.noreply.github.com>
2021-03-11 13:48:58 -0600

    Update battleship.py
    Adding AI

--- battleship.py
+++ battleship.py
@@ -1,23 +1,68 @@
 #from termcolor import colored
+import random
+import os
 
 # CREATE BOARDS
 ROWS = 10
 COLS = 10
-player1 = [] 
-ships = ['C', 'B', 'R', 'S', 'D']
-lengths = [5, 4, 3, 3, 2]
+player1 = []
+computer = []
+ships =   ['C', 'B', 'R', 'S', 'D']
+lengths = [ 5,   4,   3,   3,   2]
+direction = ["RIGHT", "DOWN"]
+
+# CLEAR THE CONSOLE
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 # POPULATE BOARD
 for i in range(ROWS): #LOOPS 10 TIMES
     row = [] 
     for j in range(COLS): #LOOPS 10 X 10 TIMES
         row.append('*')
-    player1.append(row) 
+    player1.append(row)
+    computer.append(row)
+
+# VALIDATE THAT IT IS POSSIBLE TO PLACE A SHIP
+'''
+def canPlaceShip(ship, length, direction, r, c):
+    if computer[r][c] == "*":
+        if
+    else:
+        return False
+'''    
+
+# COMPUTER PLACE SHIPS
+def placeComputerShips():
+
+    tryAgain = True
+    while tryAgain:
+        path = []
+        index = 2 # DESTROYER
+        d = random.choice(direction)
+        if d == "RIGHT":
+            r = random.randrange(0, ROWS)
+            c = random.randrange(0, COLS - 1)
+            if computer[r][c] == '*':
+                path.append([r, c])
+                index -= 1
+                for i in range(index):
+                    c += 1
+                    if computer[r][c] == '*':
+                        path.append([r, c])
+                        tryAgain = False
+                    else:
+                        tryAgain = True
+    print(path)
+    for row in path:
+        computer[row[0]][row[1]] = 'D'
+        
+        
 
 # PLACE SHIP BY ENTERING EACH ROW/COL
 def placeShipRC():
     running = True
-    while True:
+    while running:
 
         if len(ships) == 0:
             running = False
@@ -36,6 +81,8 @@ def placeShipRC():
             printBoard()
         else:
             print("Something went wrong!")
+
+            
 
 # PLACE SHIP BY ENTERING START AND END ROW / COL
 def placeShipSE():
@@ -90,10 +137,20 @@ def placeShipSE():
             printBoard()
             
 # PRINT BOARD
-def printBoard():
+def printPlayerBoard():
     r = 0
     print("  0 1 2 3 4 5 6 7 8 9 ")
     for row in player1:
+        print(r, end = " ")
+        r += 1
+        for elem in row:
+            print(elem, end = " ")
+        print()
+
+def printComputerBoard():
+    r = 0
+    print("  0 1 2 3 4 5 6 7 8 9 ")
+    for row in computer:
         print(r, end = " ")
         r += 1
         for elem in row:
@@ -114,8 +171,13 @@ def printLengths():
 
 # MAIN GAME LOOP
 print("B A T T L E S H I P !")
-printBoard()
+#printPlayerBoard()
+print()
+#printComputerBoard()
+placeComputerShips()
+printComputerBoard()
 
 # PLACE SHIPS
 #placeShipRC()
-placeShipSE()
+#placeShipSE()
+
